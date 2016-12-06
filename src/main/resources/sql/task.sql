14b916ec4a0a71d6896c6cbe47adcc445101e3e3
yongqian.liu <yongqian.liu@peraglobal.com>
2016-12-06 14:09:03 +0800

    更新：sql文件

--- src/main/resources/sql/task.sql
+++ src/main/resources/sql/task.sql
@@ -10,7 +10,12 @@ DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
   `taskId` varchar(50) NOT NULL DEFAULT '' COMMENT 'ID',
   `taskName` varchar(500) NOT NULL DEFAULT '' COMMENT '名称',
-  `groupId` varchar(500) NOT NULL DEFAULT '' COMMENT '组ID',
+  `groupId` varchar(500) NOT NULL DEFAULT '' COMMENT '组ID',  
+  `startExpress` varchar(500) NULL DEFAULT '' COMMENT '开始时间表达式',
+  `stopExpress` varchar(500) NULL DEFAULT '' COMMENT '停止时间表达式',
+  `startCommand` varchar(500) NULL DEFAULT '' COMMENT '开始命令',
+  `stopCommand` varchar(500) NULL DEFAULT '' COMMENT '结束命令',  
+  `taskState` varchar(500) NULL DEFAULT '' COMMENT '任务状态',
   PRIMARY KEY (`taskId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='任务调度器';
 
